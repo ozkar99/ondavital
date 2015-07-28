760b92503d9f76254e93cd9ab19561ca54668df8
Oscar Moreno Garza <omoreno@dalai.com>
2015-07-28 16:40:04 -0500

    adds ruboustly so if it crashes it restarts...

--- page/server.go
+++ page/server.go
@@ -3,22 +3,25 @@ package main
 import (
 	"fmt"
 	"github.com/ozkar99/ondavital"
+	"github.com/VividCortex/robustly"
 	"html/template"
 	"net/http"
 )
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		t, err := template.ParseFiles("index.html")
-		if err != nil {
-			fmt.Println("Error: ", err)
-		}
+	robustly.Run(func() {
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			t, err := template.ParseFiles("index.html")
+			if err != nil {
+				fmt.Println("Error: ", err)
+			}
 
-		q := r.FormValue("q")
-		result, _ := ondavital.Search(q)
-		t.Execute(w, result)
-	})
+			q := r.FormValue("q")
+			result, _ := ondavital.Search(q)
+			t.Execute(w, result)
+		})
 
-	http.ListenAndServe(":8080", nil)
+		http.ListenAndServe(":8080", nil)
+	}, nil)
 }
